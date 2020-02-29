81d13e9e02f387c1cc27499e3afc04997cb6e6b9
Kaan <kkureli@gmail.com>
2020-02-29 21:59:33 +0300

    done

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -1,47 +1,63 @@
-import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
-import normalBaby from './assets/unadulterated-hoglette.png'
-import SunBaby from './assets/sun-eyes.png'
-import BlueBaby from './assets/blue-eyes.png'
-import GlowingBaby from './assets/glowing-eyes.png'
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import normalBaby from "./assets/unadulterated-hoglette.png";
+import SunBaby from "./assets/sun-eyes.png";
+import BlueBaby from "./assets/blue-eyes.png";
+import GlowingBaby from "./assets/glowing-eyes.png";
 
-// is there a way we could associate eye color string values with images? 
+// is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 export default class BabyHog extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      weight: 0
+    };
   }
 
-  changeWeight = (e) => {
+  changeWeight = e => {
     // nothing needs to change here
-    const newWeight = e.target.name === "+" ? (this.state.weight + 10) : (this.state.weight - 10)
+    const newWeight =
+      e.target.name === "+" ? this.state.weight + 10 : this.state.weight - 10;
     this.setState({
       weight: newWeight
-    })
-  }
+    });
+  };
 
   render() {
+    let eyeColor = null;
+
+    let src = normalBaby;
+    if (this.props.buttonPressed === 1) {
+      eyeColor = this.props.eyeColor;
+      if (this.props.eyeColor === "glowing") {
+        src = GlowingBaby;
+      } else if (this.props.eyeColor === "sun") {
+        src = SunBaby;
+      } else if (this.props.eyeColor === "blue") {
+        src = BlueBaby;
+      }
+    }
+
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
+        <h1>Name: {this.props.name}</h1>
+        <h3>Weight:{this.state.weight}</h3>
+        <h3>Hobby:{this.props.hobby}</h3>
+        <h4>Eye Color: {eyeColor}</h4>
+
+        <Button onClick={this.changeWeight} name="+">
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button onClick={this.changeWeight} name="-">
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={src} style={{ height: "200px" }} alt="MasterBlasterJrJr" />
         </div>
-        
       </li>
-    )
+    );
   }
 }
--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,52 +1,83 @@
-import React, { Component } from 'react'
-import Master from './assets/master-hog.png'
-import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import React, { Component } from "react";
+import Master from "./assets/master-hog.png";
+import BabyHog from "./BabyHog";
+import offspring from "./db.js";
+import style from "./style.css";
+const dataFunc = data => {
+  return dataFunc.map(data => {
+    return <BabyHog name={data.name} />;
+  });
+};
 
 export default class MasterHog extends Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       eyeColor: "blue",
-    }
+      buttonPressed: 0
+    };
   }
 
-
-  changeEyeColor = (e) => {
+  changeEyeColor = e => {
     this.setState({
-      eyeColor: e.target.value
-    })
-  }
+      eyeColor: e.target.value,
+      buttonPressed: 1
+    });
+  };
 
+  renderData = list => {
+    return list.map(item => {
+      return (
+        <BabyHog
+          name={item.name}
+          hobby={item.hobby}
+          buttonPressed={this.state.buttonPressed}
+          eyeColor={this.state.eyeColor}
+        />
+      );
+    });
+  };
 
   render() {
+    let color = null;
+
+    if (this.state.buttonPressed === 1) {
+      color = this.state.eyeColor;
+    }
+
     return (
       <div>
-
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
-        Blue<br></br>
-        <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
-        Sun<br></br>
-        <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
-        Glowing<br></br>
-      
+        <div className={"buttons"}>
+          <input
+            type="radio"
+            name="eyeColor"
+            value="blue"
+            onChange={this.changeEyeColor}
+          />
+          Blue<br></br>
+          <input
+            type="radio"
+            name="eyeColor"
+            value="sun"
+            onChange={this.changeEyeColor}
+          />
+          Sun<br></br>
+          <input
+            type="radio"
+            name="eyeColor"
+            value="glowing"
+            onChange={this.changeEyeColor}
+          />
+          Glowing<br></br>
+        </div>
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
-        <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+        <h3>Eye Color: {color}</h3>
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
-        <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
-        </ul>
-
+        <ul className="hoglist">{this.renderData(offspring)}</ul>
       </div>
-    )
+    );
   }
-
 }
